79e7a21aaf8d47dbc9db287036b1d907c544c080
Kristina Muntian <kristinaflorentina01@gmail.com>
2022-10-28 15:02:01 +0200

    first commit

---
+++ 24.10.22/script/script.js
@@ -0,0 +1,58 @@
+// const name = prompt('your name');
+
+// console.log('Hello' + name);
+// console.log(`Hello ${name}`)
+
+// const r = 123;
+// const g = 32;
+// const b = 57;
+
+// console.log(`rgb(${r},${g}, ${b});`);
+// console.log('rgb('+r  + ',' + g +',' + b +');');
+
+// const value_1= 145;
+// const value_2 = 13;
+
+// console.log(value_1 + value_2);
+// console.log(value_1, value_2);
+// console.log(value_1 - value_2);
+// console.log(value_1 * value_2);
+// console.log(value_1 / value_2);
+// console.log(value_1 ** value_2);
+// console.log(value_1 % value_2);
+
+
+
+
+
+// const value_1  = '12';
+// const value_2 = 12;
+// console.log(typeof ((+value_1)));               //плюс преобразует строку в число
+// console.log(typeof(''+value_2));             //преобразует в строку
+
+
+
+
+
+//напишите программу, которая через prompt получает два числа и выводит в консоль их сумму
+
+// const sum = prompt('1 number');           //вводим в поля числа которые будем складывать
+// const sum1 = prompt('2 number');
+// console.log(+sum + +sum1);                 //складываем 2 числа
+
+// let value_1 = prompt('enter first number');
+// console.log(typeof(+value_1));
+
+
+
+
+
+// написать программу, которая запрашивает у пользователя сколько ему лет и выводит
+// сколько ему минут
+
+let age = prompt('how old are you');
+console.log(age * 365 * 24 *60)
+
+
+
+
